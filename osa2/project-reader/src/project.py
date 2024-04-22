d9e9c1b455833ad75ed1dbf73a9e8e5dd8f1be26
Elias Brunila <brunilaelias@gmail.com>
2024-04-22 19:13:56 +0300

    riippuvuuksien harjoittelua

--- osa2/project-reader/src/project.py
+++ osa2/project-reader/src/project.py
@@ -1,17 +1,28 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
-        self.name = name
-        self.description = description
-        self.dependencies = dependencies
-        self.dev_dependencies = dev_dependencies
+    def __init__(self, tiedot):
+        self.tiedot = tiedot
 
     def _stringify_dependencies(self, dependencies):
         return ", ".join(dependencies) if len(dependencies) > 0 else "-"
 
     def __str__(self):
-        return (
-            f"Name: {self.name}"
-            f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
-        )
+        s = ""
+        a = "Authors: \n"
+        depe = "Dependencies: \n"
+        d = "Development dependencies: \n"
+        for key in self.tiedot:
+            if isinstance(self.tiedot[key], str):
+                s += key.capitalize() + ": " + self.tiedot[key] + "\n"
+                continue
+            if key == "authors":
+                 for i in self.tiedot[key]:
+                     a += "- " + i + "\n"
+                 continue
+            if key == "dependencies":
+                for key in self.tiedot[key]:
+                    depe += "- " + key + "\n"
+                continue
+
+        for key in self.tiedot['group']['dev']['dependencies']:
+            d += "- " + key + "\n"
+        return s + "\n" + a + "\n" + depe + "\n" + d
--- osa2/project-reader/src/project_reader.py
+++ osa2/project-reader/src/project_reader.py
@@ -1,4 +1,5 @@
 from urllib import request
+import toml
 from project import Project
 
 
@@ -7,9 +8,7 @@ class ProjectReader:
         self._url = url
 
     def get_project(self):
-        # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
-
-        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        c = toml.loads(content)
+        tiedot = c["tool"]["poetry"]
+        return Project(tiedot)
